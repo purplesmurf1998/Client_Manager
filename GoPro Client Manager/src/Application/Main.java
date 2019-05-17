182861d8effe2b09b378d5979acc71b9c9c06657
Cedrik Dubois <cedrikdubois.edu@gmail.com>
2019-05-16 23:02:12 -0400

    Functioning Database backup
    Database now gets backed up to the amazon database once a week given the app is launched at least once a week.

--- GoPro Client Manager/src/Application/Main.java
+++ GoPro Client Manager/src/Application/Main.java
@@ -49,7 +49,7 @@ public class Main extends Application {
         System.out.println("Checking for backup");
         if (checkForBackup()){
             System.out.println("Check returned true, backup database");
-            //backupDatabase();
+            backupDatabase();
         }
         
         loginPage = new LoginPage(local_connection, this);
@@ -213,7 +213,6 @@ public class Main extends Application {
             
             calendar.setTime(date_backup);
             week_backup = calendar.get(Calendar.WEEK_OF_YEAR);
-            System.out.println();
             
             if (week_now != week_backup)
                 return true;
